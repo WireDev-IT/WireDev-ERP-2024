0979336a72095e194802d589cb0358ce513afc10
tomo2403 <49612544+tomo2403@users.noreply.github.com>
2023-01-04 13:17:38 +0100

    added new models for stats

---
+++ WireDev.Erp.V2024.Models/Statistics/CanceledOnDate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WireDev.Erp.V1.Models.Statistics
+{
+	public class SoldOnDate : EventOnDate
+	{
+        public SoldOnDate(DateTime date, Guid productId) : base(date, productId)
+        {
+        }
+    }
+}
+
