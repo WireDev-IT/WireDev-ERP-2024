41d760bbda90313e871f30b671a6300fc92220db
tomo2403 <49612544+tomo2403@users.noreply.github.com>
2023-01-06 23:59:03 +0100

    extendet models

---
+++ WireDev.ERP.V2024.Api/Context/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WireDev.Erp.V1.Api.Context
+{
+    public class ApplicationUserDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationUserDbContext(DbContextOptions<ApplicationUserDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ WireDev.Erp.V2024.Models/Interfaces/IProductStats.cs
@@ -0,0 +1,17 @@
+﻿using WireDev.Erp.V1.Models.Enums;
+
+namespace WireDev.Erp.V1.Models.Statistics
+{
+    public interface IProductStats
+    {
+        Guid ProductId { get; }
+        Guid PriceId { get; }
+        uint SoldItems { get; }
+        uint CanceledItems { get; }
+        uint RefundedItems { get; }
+        uint DisposedItems { get; }
+        Dictionary<long, TransactionType> Transactions { get; }
+
+        void AddTransaction(TransactionType type);
+    }
+}
